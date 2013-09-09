6b5a7dcc273faa61429ea93c6c3688d73c1028a0
Clayton Hunt <claytonhunt_104@hotmail.com>
2013-09-09 19:33:15 -0400

    Added Big Green OK to build script to let me know it's OK

--- build/Jakefile.js
+++ build/Jakefile.js
@@ -1,8 +1,25 @@
-desc("Example!");
-task("example", ["dependency"], function () {
-    console.log("example task");
+"use strict";
+
+desc("Build Everything");
+task("default", ["dependency"], function () {
+    console.log("");
+    console.log(colors.green + "------");
+    console.log("* OK *");
+    console.log("------" + colors.reset);
 });
 
 task("dependency", function () {
     console.log("dependency");
 });
+
+var colors = {
+    black: '\u001b[30m',
+    red: '\u001b[31m',
+    green: '\u001b[32m',
+    yellow: '\u001b[33m',
+    blue: '\u001b[34m',
+    magenta: '\u001b[35m',
+    cyan: '\u001b[36m',
+    white: '\u001b[37m',
+    reset: '\u001b[0m'
+};
