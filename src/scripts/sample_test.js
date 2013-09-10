06af1a88f5a492540c73fb1bf3aca7b0d9e13cb8
Clayton Hunt <claytonhunt_104@hotmail.com>
2013-09-09 22:39:01 -0400

    Added sample client test

---
+++ src/scripts/sample_test.js
@@ -0,0 +1,10 @@
+/*globals describe, it, expect */
+(function () {
+    "use strict";
+
+    describe('Something', function () {
+        it('does stuff', function () {
+            expect(1).toBe(1);
+        });
+    });
+}());
