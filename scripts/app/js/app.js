b33d20e983f5bae20f86ffc31dd17e00fb94a05f
Clayton Hunt <clayton.hunt@digitalcomply.com>
2014-12-01 20:49:17 -0500

    Testing Angular

---
+++ scripts/app/js/app.js
@@ -0,0 +1,31 @@
+﻿var Agile = Agile || {};
+
+(function (mod) {
+    mod.Sprint = function (data) {
+        var self = this;
+
+        data = data || { };
+
+        self.start = data.start || '*';
+        self.end = data.end || '*';
+    };
+}(Agile));
+
+(function () {
+    'use strict';
+
+    angular.module('agileApp', [])
+           .value('version', '0.0.1');
+}());
+
+(function (mod) {
+    'use strict';
+
+    angular.module('agileApp').controller('KanbanCtrl', ['$scope', function ($scope) {
+        $scope.sprints = [
+            new mod.Sprint(),
+            new mod.Sprint(),
+            new mod.Sprint()
+        ];
+    }]);
+}(Agile));
---
+++ scripts/app/tests/spec/app_spec.js
@@ -0,0 +1,89 @@
+﻿/* globals describe, it*/
+
+(function (mod) {
+    'use strict';
+
+    describe('App: agileApp', function () {
+        beforeEach(module('agileApp'));
+
+        it('is version 0.0.1', inject(function (version) {
+            expect(version).toBe('0.0.1');
+        }));
+
+        describe('Controller: KanbanCtrl', function () {
+            var scope;
+            var ctrl;
+
+            beforeEach(inject(function ($rootScope, $controller) {
+                scope = $rootScope.$new();
+                ctrl = $controller('KanbanCtrl', {
+                    $scope: scope
+                });
+            }));
+
+            it('exists', function () {
+                expect(ctrl).toBeDefined();
+            });
+
+            describe('Kanban: sprints', function () {
+                it('is defined', function() {
+                    expect(scope.sprints).toBeDefined();
+                });
+
+                it('is an array', function() {
+                    expect(scope.sprints instanceof Array).toBeTruthy();
+                });
+
+                it('is populated on controller load', function() {
+                    expect(scope.sprints.length).toBe(3);
+                });
+
+                it('is populated with sprints', function() {
+                    expect(scope.sprints[0] instanceof mod.Sprint).toBeTruthy();
+                });
+            });
+        });
+    });
+
+    describe('Agile: Sprint', function() {
+        var sprint;
+
+        beforeEach(function() {
+            sprint = new mod.Sprint();
+        });
+
+        it('is defined', function() {
+            expect(mod.Sprint).toBeDefined();
+        });
+
+        describe('start', function() {
+            it('has start', function () {
+                expect(sprint.start).toBeDefined();
+            });
+
+            it('defaults start to *', function () {
+                expect(sprint.start).toBe('*');
+            });
+
+            it('populates start from data', function () {
+                var s = new mod.Sprint({ start: '01/01/2014' });
+                expect(s.start).toBe('01/01/2014');
+            });
+        });
+
+        describe('end', function() {
+            it('has end', function() {
+                expect(sprint.end).toBeDefined();
+            });
+
+            it('defaults end to *', function() {
+                expect(sprint.end).toBe('*');
+            });
+
+            it('populates end from data', function() {
+                var s = new mod.Sprint({ end: '01/01/2014' });
+                expect(s.end).toBe('01/01/2014');
+            });
+        });
+    });
+}(Agile));
